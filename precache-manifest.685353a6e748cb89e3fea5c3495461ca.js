dfb2d06e3d44e7397d1aa92ed59b740192817211
QuyetAQ <quyetaq@gmail.com>
2021-06-20 10:23:56 +0700

    Update Theme

---
+++ precache-manifest.685353a6e748cb89e3fea5c3495461ca.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "38ad23ee3b94ad647747",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "06f9070b638b618e3d8142e0e76cb28e",
+    "url": "index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.21778044e688298872ffd71f2f011d56.js"
+  "precache-manifest.685353a6e748cb89e3fea5c3495461ca.js"
 );
 
 /**
