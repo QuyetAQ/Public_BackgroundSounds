bd220f9e120eeda20f339c1b898968f9fa3be61c
QuyetAQ <quyetaq@gmail.com>
2021-06-20 09:58:40 +0700

    First commit

---
+++ precache-manifest.21778044e688298872ffd71f2f011d56.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac517b644fd8a8e0424",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "06f9070b638b618e3d8142e0e76cb28e",
+    "url": "index.html"
+  }
+];
